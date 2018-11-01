a5c45107a2b9a37cb018ff806de07c634d887d11
CarlosCL98 <camer.cl98@gmail.com>
2018-11-01 10:58:54 -0500

    Modificando carpetas

--- BDBancoIniciativas.sql
+++ BDBancoIniciativas.sql
@@ -1,12 +1,12 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2018-10-31 18:18:09.37
+-- Last modification date: 2018-11-01 15:50:35.266
 
 -- tables
 -- Table: Area
 CREATE TABLE Area (
     id int  NOT NULL,
     nombre varchar(20)  NOT NULL,
-    CONSTRAINT Area_ak_nombre UNIQUE (nombre),
+    CONSTRAINT Area_ak_nombre UNIQUE (nombre) NOT DEFERRABLE  INITIALLY IMMEDIATE,
     CONSTRAINT Area_pk PRIMARY KEY (id)
 );
 
@@ -14,7 +14,7 @@ CREATE TABLE Area (
 CREATE TABLE Estado (
     id int  NOT NULL,
     descripcion varchar(30)  NOT NULL,
-    CONSTRAINT Estado_ak_descripcion UNIQUE (descripcion),
+    CONSTRAINT Estado_ak_descripcion UNIQUE (descripcion) NOT DEFERRABLE  INITIALLY IMMEDIATE,
     CONSTRAINT Estado_pk PRIMARY KEY (id)
 );
 
@@ -26,6 +26,7 @@ CREATE TABLE Iniciativa (
     detalle varchar(500)  NOT NULL,
     fechaCreacion date  NOT NULL,
     Usuario_correo varchar(50)  NOT NULL,
+    Estado_id int  NOT NULL,
     CONSTRAINT Iniciativa_pk PRIMARY KEY (id)
 );
 
@@ -43,7 +44,7 @@ CREATE TABLE Interes (
 CREATE TABLE Rol (
     id int  NOT NULL,
     rol varchar(20)  NOT NULL,
-    CONSTRAINT Rol_ak_rol UNIQUE (rol),
+    CONSTRAINT Rol_ak_rol UNIQUE (rol) NOT DEFERRABLE  INITIALLY IMMEDIATE,
     CONSTRAINT Rol_pk PRIMARY KEY (id)
 );
 
@@ -54,10 +55,10 @@ CREATE TABLE Usuario (
     correo varchar(50)  NOT NULL,
     codigo int  NOT NULL,
     fechaNacimiento date  NOT NULL,
+    estado varchar(10)  NOT NULL,
     Rol_id int  NOT NULL,
     Area_id int  NOT NULL,
-    Estado_id int  NOT NULL,
-    CONSTRAINT Usuario_ak_codigo UNIQUE (codigo),
+    CONSTRAINT Usuario_ak_codigo UNIQUE (codigo) NOT DEFERRABLE  INITIALLY IMMEDIATE,
     CONSTRAINT Usuario_pk PRIMARY KEY (correo)
 );
 
@@ -72,49 +73,65 @@ CREATE TABLE Voto (
 -- Reference: Comentario_Iniciativa (table: Interes)
 ALTER TABLE Interes ADD CONSTRAINT Comentario_Iniciativa
     FOREIGN KEY (Iniciativa_id)
-    REFERENCES Iniciativa (id)
+    REFERENCES Iniciativa (id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
 ;
 
 -- Reference: Comentario_Usuario (table: Interes)
 ALTER TABLE Interes ADD CONSTRAINT Comentario_Usuario
     FOREIGN KEY (Usuario_correo)
-    REFERENCES Usuario (correo)
+    REFERENCES Usuario (correo)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: Iniciativa_Estado (table: Iniciativa)
+ALTER TABLE Iniciativa ADD CONSTRAINT Iniciativa_Estado
+    FOREIGN KEY (Estado_id)
+    REFERENCES Estado (id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
 ;
 
 -- Reference: Iniciativa_Usuario (table: Iniciativa)
 ALTER TABLE Iniciativa ADD CONSTRAINT Iniciativa_Usuario
     FOREIGN KEY (Usuario_correo)
-    REFERENCES Usuario (correo)
+    REFERENCES Usuario (correo)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
 ;
 
 -- Reference: Usuario_Area (table: Usuario)
 ALTER TABLE Usuario ADD CONSTRAINT Usuario_Area
     FOREIGN KEY (Area_id)
-    REFERENCES Area (id)
-;
-
--- Reference: Usuario_Estado (table: Usuario)
-ALTER TABLE Usuario ADD CONSTRAINT Usuario_Estado
-    FOREIGN KEY (Estado_id)
-    REFERENCES Estado (id)
+    REFERENCES Area (id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
 ;
 
 -- Reference: Usuario_Rol (table: Usuario)
 ALTER TABLE Usuario ADD CONSTRAINT Usuario_Rol
     FOREIGN KEY (Rol_id)
-    REFERENCES Rol (id)
+    REFERENCES Rol (id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
 ;
 
 -- Reference: Voto_Iniciativa (table: Voto)
 ALTER TABLE Voto ADD CONSTRAINT Voto_Iniciativa
     FOREIGN KEY (Iniciativa_id)
-    REFERENCES Iniciativa (id)
+    REFERENCES Iniciativa (id)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
 ;
 
 -- Reference: Voto_Usuario (table: Voto)
 ALTER TABLE Voto ADD CONSTRAINT Voto_Usuario
     FOREIGN KEY (Usuario_correo)
-    REFERENCES Usuario (correo)
+    REFERENCES Usuario (correo)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
 ;
 
 -- End of file.
